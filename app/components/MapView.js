0713899b77f2010a3fc3c19877125a2247fc7601
TanyaShved <tankonatanke90@gmail.com>
2020-08-05 22:35:07 +0300

    npm to yarn, fix old leaflet interface

--- app/components/MapView.js
+++ app/components/MapView.js
@@ -16,10 +16,10 @@ export default Marionette.View.extend({
 
     opm.control.mousePosition().addTo(map)
     opm.Control.measureControl().addTo(map)
-    opm.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    opm.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
-      id: 'mapbox.streets',
+      id: 'mapbox/streets-v11',
       accessToken: 'pk.eyJ1IjoidGFua2FzaHZlZCIsImEiOiJja2RocGxuNWgwNWNiMnJvZW9sd3BudWVtIn0.Vz1nUFZ8o_uNGdJrXaSBCA'
     }).addTo(map)
 
